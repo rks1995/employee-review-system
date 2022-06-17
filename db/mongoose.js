70b489310c23ca81704d2a3b892e5aeda903fac6
Senjam RatnaKumar <sratnakumar99@gmail.com>
2022-06-17 09:09:09 +0530

    mongodb connection successfull

---
+++ db/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const url =
+  'mongodb+srv://savio:1234@nodeexpressprojects.vq35t.mongodb.net/14-Employee?retryWrites=true&w=majority'
+
+const db = mongoose
+  .connect(url)
+  .then(() => {
+    console.log('connection successfull')
+  })
+  .catch((err) => {
+    console.log('err in connecting db')
+  })
+
+module.exports = db
