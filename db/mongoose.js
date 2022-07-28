df4a03c7ba42cf594f5cb84987c22b9aa1cbc827
Senjam RatnaKumar <sratnakumar99@gmail.com>
2022-07-28 12:55:07 +0530

    connect-db

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,11 +1,8 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-const url =
-  'mongodb+srv://savio:1234@nodeexpressprojects.vq35t.mongodb.net/14-Employee?retryWrites=true&w=majority'
-
 const db = mongoose
-  .connect(url)
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('connection successfull')
   })
